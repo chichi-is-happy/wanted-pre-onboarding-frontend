1ecb339408e0b95672dcc5767fb6531e5edc4ad0
chichi <seonrye.yun@gmail.com>
2023-06-21 04:36:46 +0900

    feat: 이메일, 비밀번호 유효성 검사 기능 구현

--- src/components/sign/Sign.tsx
+++ src/components/sign/Sign.tsx
@@ -1,6 +1,56 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 
+type signInputType = {
+  email: {
+    value: string;
+    isValid: boolean;
+  };
+  password: {
+    value: string;
+    isValid: boolean;
+  };
+};
+
+const signInitialData: signInputType = {
+  email: {
+    value: "",
+    isValid: false,
+  },
+  password: {
+    value: "",
+    isValid: false,
+  },
+};
 const Sign = () => {
+  const [signInput, setSignInput] = useState<signInputType>(signInitialData);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>, label: string) => {
+    // 정규표현식
+    const emailRegex = /@/;
+
+    const { value } = e.target!;
+    if (
+      (label === "email" && emailRegex.test(value)) ||
+      (label === "password" && value.length >= 8)
+    ) {
+      setSignInput({
+        ...signInput,
+        [label]: { value: value, isValid: true },
+      });
+      console.log("변경 확인", label, signInput);
+
+      return;
+    } else {
+      setSignInput({
+        ...signInput,
+        [label]: { value: value, isValid: false },
+      });
+      console.log("미통과 변경 확인", label, signInput);
+
+      return;
+    }
+  };
+
   return (
     <React.Fragment>
       <main className="base_layout">
@@ -15,15 +65,32 @@ const Sign = () => {
           <form className="flex w-3/5 flex-col p-10 h-full justify-center">
             <div className="flex flex-col input-field-wrapper mb-3">
               <label className="username">아이디</label>
-              <input data-testid="email-input" />
+              <input
+                data-testid="email-input"
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  handleChange(e, "email")
+                }
+              />
             </div>
 
             <div className="flex flex-col input-field-wrapper mb-6">
               <label className="password">비밀번호</label>
-              <input data-testid="password-input" />
+              <input
+                data-testid="password-input"
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  handleChange(e, "password")
+                }
+              />
             </div>
 
-            <button data-testid="signin-button" className="base_button mt-6">
+            <button
+              data-testid="signin-button"
+              className="
+              base_button mt-6
+              disabled:opacity-50"
+              type="submit"
+              disabled={!signInput.email.isValid || !signInput.password.isValid}
+            >
               로그인
             </button>
             <p className="mt-7">계정이 없으신가요? 지금 바로 만들어보세요</p>
