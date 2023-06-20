4ccab5a87d90bc61f17d917632c8fa2b0716754f
chichi <seonrye.yun@gmail.com>
2023-06-21 00:51:21 +0900

    feat: 기본 세팅 및 메인 로그인 화면 UI 추가

---
+++ src/components/sign/Sign.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Sign = () => {
+  return (
+    <React.Fragment>
+      <main className="base_layout">
+        <div className="flex h-full items-center">
+          <div className="flex flex-col w-2/5 p-10 border-r h-full justify-center ">
+            <div className="flex-col">
+              <h1 className="text-xl text-red-200">어서오세요</h1>
+              <p>로그인이 필요합니다</p>
+            </div>
+          </div>
+
+          <form className="flex w-3/5 flex-col p-10 h-full justify-center">
+            <div className="flex flex-col input-field-wrapper mb-3">
+              <label className="username">아이디</label>
+              <input data-testid="email-input" />
+            </div>
+
+            <div className="flex flex-col input-field-wrapper mb-6">
+              <label className="password">비밀번호</label>
+              <input data-testid="password-input" />
+            </div>
+
+            <button data-testid="signin-button" className="base_button mt-6">
+              로그인
+            </button>
+            <p className="mt-7">계정이 없으신가요? 지금 바로 만들어보세요</p>
+            <button className="base_button"> 회원 가입하기</button>
+          </form>
+        </div>
+      </main>
+    </React.Fragment>
+  );
+};
+
+export default Sign;
---
+++ src/pages/Signin.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sign from "../components/sign/Sign";
+
+const SignIn = () => {
+  return (
+    <React.Fragment>
+      <Sign />
+    </React.Fragment>
+  );
+};
+
+export default SignIn;
