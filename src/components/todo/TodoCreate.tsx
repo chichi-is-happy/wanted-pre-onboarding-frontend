5d48356e0b89e82e99d7e858967c28ccf049ea07
chichi <seonrye.yun@gmail.com>
2023-07-12 04:24:30 +0900

    fix: 한글로 입력한 TODO를 엔터키 입력으로 추가할 때 맨 끝 글자만 전송되는 문제 수정

--- src/components/todo/TodoCreate.tsx
+++ src/components/todo/TodoCreate.tsx
@@ -1,21 +1,29 @@
-import React, { useContext, useRef } from "react";
+import React, { useContext, useRef, useState, ChangeEvent } from "react";
 import { dispatchContext } from "../../context/todoContexts";
-
 const TodoCreate = () => {
+  const [inputTodo, setInput] = useState("");
+  const [isComposing, setComposing] = useState(false);
   const todos = useContext(dispatchContext);
-  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+    console.log(inputTodo);
+  };
 
   const handleButtonClick = () => {
-    const inputValue = inputRef.current?.value;
-    if (inputValue) {
-      todos.addTodo(inputValue);
-      inputRef.current.value = "";
-    } else alert("할 일을 입력해주세요!");
+    if (inputTodo) {
+      todos.addTodo(inputTodo);
+      setInput("");
+    } else {
+      alert("할 일을 입력해주세요!");
+    }
   };
 
   const submitOnEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (isComposing) return;
     if (e.key === "Enter") {
       handleButtonClick();
+      console.log("submitOnEnter 작동");
     }
   };
 
@@ -23,12 +31,17 @@ const TodoCreate = () => {
     <div className="flex w-full mb-8">
       <>
         <input
+          type="text"
           className="flex-grow mr-2"
           data-testid="new-todo-input"
           onKeyDown={submitOnEnter}
-          ref={inputRef}
+          onCompositionStart={() => setComposing(true)}
+          onCompositionEnd={() => setComposing(false)}
+          value={inputTodo}
+          onChange={(e) => handleChange(e)}
         />
         <button
+          type="button"
           className="w-1/5 base_button"
           data-testid="new-todo-add-button"
           onClick={handleButtonClick}
